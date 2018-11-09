5f058b80f3d061151da36aee114faf797ffa94ad
lord8266 <lordvishwa123@gmail.com>
2018-11-09 21:19:37 +0530

    Added simple player enemy

--- word_game.cpp
+++ word_game.cpp
@@ -27,7 +27,7 @@ bool check_word(string word,string base) {
 
     if (word[i]==base[i+off]) {
       i++;
-      off++; //if letter matches move to next letter
+       //if letter matches move to next letter
       //since they are sorted they will match in an order
     }
     else
@@ -109,17 +109,79 @@ void printLettersInAGrid(const string& base)
     }
 
 }
+
+void print_health(int *hp) { //prints health of both
+  cout<<"\n++++++++++++ Health +++++++++++++\n";
+  cout<<"player ";
+  for (int i=0;i<hp[0];i++) {
+    cout<<"=";
+  }
+  cout<<"\nenemy  ";
+  for (int i=0;i<hp[1];i++) {
+    cout<<"=";
+  }
+  cout<<"\n+++++++++++++++++++++++++++++++++\n"<<endl;
+}
+int enemy_damage(int* player_health) {
+  *player_health-=7;
+}
+
+int player_damage(const string& word,int *enemy_health) {
+  *enemy_health-= word.size()*3;
+  //outputs damage based on word size multiplied by 3
+  //so more bigger word your damage increases three folds
+}
+bool check_complete(int* health) {
+  bool ret=1;
+  if(health[0]<=0) {
+    cout<<"enemy wins "<<endl;
+  }
+  else if(health[1]<=0) {
+    cout<<"player wins"<<endl;
+  }
+  else {
+    ret=0;
+  }
+return ret;
+
+}
 int main()
 {
+  int hp[2] {50,50};//simple player enemy
+  // corresponding to player hp and enemy hp respectively
+  //as of now only one enemy
     makeRandomLetters(base);
     readFile(dictionary);
-    string temp;
+    string word;
 
-    while (cin>>temp) {
-    if(dictionary.find(temp)!=dictionary.end()) {
+    print_health(hp);
+
+    while (cin>>word) {
+      if(word==":reset") { //if you want to reset the word
+        makeRandomLetters(base);
+        enemy_damage(&hp[0]); //if words ned to be reset you lose your turn
+        print_health(hp);
+        printLettersInAGrid(base);
+        continue;
+      }
+      if(word==":print") {//if you want to print the letters again and dont
+                           //want to scroll up
+        printLettersInAGrid(base);
+        continue;
+      }
+
+    if(dictionary.find(word)!=dictionary.end()) {
       //check if the word is present in the dictionary
-      if(check_word(temp,base))
+      if(check_word(word,base)) {
         cout<<"word possible"<<endl;
+        player_damage(word,&hp[1]); //hp[1] refers to enemy health
+        if(check_complete(hp)) break; //if helath gone berlow zero end the game
+        enemy_damage(&hp[0]); //hp[0] is the player health
+        if(check_complete(hp)) break;
+
+        print_health(hp);
+
+  }
       else
         cout<<"word not possible"<<endl;
     }
